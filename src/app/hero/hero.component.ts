b3f1748566e380c4883a15e0db817f29a08d64f2
Renjitha26 <renjitha269@gmail.com>
2025-02-27 10:32:16 +0530

    hero-component-created

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 
 @Component({
   selector: 'app-hero',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule,MatCardModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
